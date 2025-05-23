fd73c2645557cd3f88e33cd7156f13e465ce4713
Nickywest21 <158302385+Nickywest21@users.noreply.github.com>
2025-05-23 08:24:41 -0600

    Paula Nicolle West Oritz - 1245524 - Parcial

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,27 @@
 package ed.lab;
 
-import java.util.List;
+import java.util.*;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals.isEmpty()) {
+            return 0;
+        }
 
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
+
+        Queue<Integer> minHeap = new PriorityQueue<>();
+
+        for (MeetingInterval meeting : meetingIntervals) {
+            if (!minHeap.isEmpty() && meeting.startTime() >= minHeap.peek()) {
+                minHeap.poll();
+            }
+            minHeap.add(meeting.endTime());
+        }
+
+        return minHeap.size();
+    }
 }
+
+
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,22 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E02Logger {
+    private Map<String, Integer> map;
 
     public E02Logger() {
+        map = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
-        return false;
-    }
+        if (map.containsKey(message) && timestamp - map.get(message) < 10) {
+            return false;
+        }
+        map.put(message, timestamp);
+        return true;
 
+    }
 }
+
